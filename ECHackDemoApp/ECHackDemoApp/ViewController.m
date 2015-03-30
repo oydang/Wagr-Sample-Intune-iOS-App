aaba4a5a131075184111b9d110d4719184b9af84
philgetzen <pgetz8@gmail.com>
2015-03-30 13:42:58 -0400

    Added timer functionality

--- ECHackDemoApp/ECHackDemoApp/ViewController.m
+++ ECHackDemoApp/ECHackDemoApp/ViewController.m
@@ -11,12 +11,13 @@
 @interface ViewController ()
 
 @end
-
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    timerRunning = NO;
+    pauseTimeInterval = 0;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,11 +26,39 @@
 }
 
 - (IBAction)onClockButtonPressed:(UIButton *)sender {
-    if ([self.clockButton.currentTitle isEqualToString:@"Clock In"]){
+    if (!timerRunning){
         [self.clockButton setTitle:@"Clock Out" forState:UIControlStateNormal];
+        
+        timerRunning = YES;
+        
+        self.startDate = [NSDate date] ;
+        self.startDate = [self.startDate dateByAddingTimeInterval:((-1)*(pauseTimeInterval))];
+        self.timeWorkedTimer = [NSTimer scheduledTimerWithTimeInterval:1.0/10.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
     }
     else{
         [self.clockButton setTitle:@"Clock In" forState:UIControlStateNormal];
+        
+        timerRunning = NO;
+        [self.timeWorkedTimer invalidate];
+        self.timeWorkedTimer = nil;
+        [self updateTimer];
     }
+}
+
+-(void) updateTimer {
+    
+    NSDate *currentDate = [NSDate date];
+    
+    NSTimeInterval timeInterval = [currentDate timeIntervalSinceDate:self.startDate];
+    NSDate *timerDate = [NSDate dateWithTimeIntervalSince1970:timeInterval];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"HH:mm"];
+    [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0.0]];
+    
+    NSString *timeString = [dateFormatter stringFromDate:timerDate];
+    self.timeWorkedLabel.text = timeString;
+    pauseTimeInterval = timeInterval;
+    
 }
 @end
